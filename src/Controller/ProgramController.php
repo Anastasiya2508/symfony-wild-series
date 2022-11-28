4a9e542d683d34ab369f6c4152402a632537726f
Anastasiya_Fernandez <anastasiya.ustych@gmail.com>
2022-11-28 16:42:24 +0100

    La page d'accueil de site

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+
+    #[Route('/{id}/', name: 'show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(int $id = 1): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => 4]);
+    }
+}
